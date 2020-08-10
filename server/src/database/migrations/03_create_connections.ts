9a24083a38885dcda5bc16efcb59612cfb2eb5a6
KEVIN_NOGUEIRA <kevinnog@outlook.com.br>
2020-08-09 22:27:32 -0300

    Day 2 final touches

--- server/src/database/migrations/03_create_connections.ts
+++ server/src/database/migrations/03_create_connections.ts
@@ -1,7 +1,7 @@
 import Knex from "knex";
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable("connetions", (table) => {
+  return knex.schema.createTable("connections", (table) => {
     table.increments("id").primary();
 
     table.integer("user_id").notNullable().references("id").inTable("users").onUpdate("CASCADE").onDelete("CASCADE");
@@ -11,5 +11,5 @@ export async function up(knex: Knex) {
 }
 
 export async function down(knex: Knex) {
-  return knex.schema.dropTable("connetions");
+  return knex.schema.dropTable("connections");
 }
