222cbd71bb8b9ffb952308a37d6d1ad238d12354
KEVIN_NOGUEIRA <kevinnog@outlook.com.br>
2020-08-13 23:04:44 -0300

    Day 4 - mobile

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { View } from "react-native";
+import PageHeader from "../../components/PageHeader";
+
+import styles from "./styles";
 
 function Favorites() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+    </View>
+  );
 }
 
 export default Favorites;
